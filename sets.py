e153eebee9043011bbf69d826f8fb3eed08b7876
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-05-30 22:55:55 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,15 +2,15 @@
 
 
     def clean_ingredients(dish_name, dish_ingredients):
-    dish_ingr_set= set(dish_ingredients)
-    fdish= dish_name, dish_ingr_set
-    return fdish   
-
-
-
+        dish_ingr_set= set(dish_ingredients)
+        fdish= dish_name, dish_ingr_set
+        return fdish   
+    
+    
+    
     def check_drinks(drink_name, drink_ingredients):
-    if len(set(drink_ingredients.intersection(ALCOHOLS))) != 0:
-        return f"{drink_name} Cocktail"
-    else:
-        return f"{drink_name} Mocktail"
+        if len(set(drink_ingredients.intersection(ALCOHOLS))) != 0:
+            return f"{drink_name} Cocktail"
+        else:
+            return f"{drink_name} Mocktail"
 
