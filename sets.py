be022610492a80f7a482a6c4d96bbaa070fba415
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-05-30 22:55:23 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,14 +1,14 @@
     from sets_categories_data import ALCOHOLS 
 
 
-def clean_ingredients(dish_name, dish_ingredients):
+    def clean_ingredients(dish_name, dish_ingredients):
     dish_ingr_set= set(dish_ingredients)
     fdish= dish_name, dish_ingr_set
     return fdish   
 
 
 
-def check_drinks(drink_name, drink_ingredients):
+    def check_drinks(drink_name, drink_ingredients):
     if len(set(drink_ingredients.intersection(ALCOHOLS))) != 0:
         return f"{drink_name} Cocktail"
     else:
